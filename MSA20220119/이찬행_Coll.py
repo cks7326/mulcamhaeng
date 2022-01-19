db83e4e2c72d037959c1bb1fe97def35d32acc9d
cks7326 <62586656+cks7326@users.noreply.github.com>
2022-01-19 17:34:53 +0900

    Add files via upload

--- MSA20220119/이찬행_Coll.py
+++ MSA20220119/이찬행_Coll.py
@@ -1,1 +1,23 @@
+#1
+avg = {'국어':95,'영어':90,'수학':80, '과학':50}
+
+average = sum(avg.values())/len(avg)
+
+temp = 0
+
+#2
+
+ans2 = {i for i in range(1,101) if i%3==0} 
+ans3 = {i for i in range(1,101) if i%5==0} 
+ans = ans2 | ans3
+
+temp2 = 0
+
+#3
+ans4 = [i for i in range(7,-9,-2)]
+
+temp3 = 0
+
+#4
+ans5 = tuple(ans4)
 
