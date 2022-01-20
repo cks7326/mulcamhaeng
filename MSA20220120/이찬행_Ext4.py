df2413ac1e3b348129ba5f09b1ca06bcf5d51a74
cks7326 <62586656+cks7326@users.noreply.github.com>
2022-01-20 18:04:04 +0900

    Update 이찬행_Ext4.py

--- MSA20220120/이찬행_Ext4.py
+++ MSA20220120/이찬행_Ext4.py
@@ -14,21 +14,30 @@ if len(hong1) > 1 :
     for i in range(len(hong1)):
         sum1.append(hong1[i])
         sum_all += sum1[i]
+        if (i+1) == 4:
+            avg1 = sum1/(i+1)
         
 
 if len(hong2) > 1 :
     for i in range(len(hong2)):
         sum2.append(hong2[i])
         sum_all += sum2[i]
+        if (i+1) == 4:
+            avg2 = sum2/(i+1)
 
 if len(hong3) > 1 :
     for i in range(len(hong3)):
         sum3.append(hong3[i])
         sum_all += sum3[i]
+        if (i+1) == 4:
+            avg3 = sum3/(i+1)
 
 if len(hong4) > 1 :
     for i in range(len(hong4)):
         sum4.append(hong4[i])
         sum_all += sum4[i]
+        if (i+1) == 4:
+            avg4 = sum4/(i+1)
+
 
 
